88e441ece23d19befc2575717f0cac0f9cabfb1d
Chase Ridenour <cridenour@tgen.org>
2022-08-30 13:35:27 -0400

    registered formats within formats.py instead of types

--- q2_nasp2_types/formats.py
+++ q2_nasp2_types/formats.py
@@ -1,4 +1,5 @@
 from qiime2.plugin import model
+from q2_nasp2_types.plugin_setup import plugin
 
 
 class BWAIndexFileFormat(model.BinaryFileFormat):
@@ -21,6 +22,7 @@ class SAMFileFormat(model.TextFileFormat):
     def _validate_(self, level):
         pass
 
+
 class SAMFileDirFmt(model.DirectoryFormat):
     sam_files = model.FileCollection(r'*.sam', format=SAMFileFormat)
 
@@ -29,14 +31,15 @@ class BAMFileFormat(model.TextFileFormat):
     def _validate_(self, level):
         pass
 
+
 class BAMFileDirFmt(model.DirectoryFormat):
     sam_files = model.FileCollection(r'*.sam', format=BAMFileFormat)
-
 
 
 class VCFFileFormat(model.TextFileFormat):
     def _validate_(self, level):
         pass
+
 
 class VCFFileDirFmt(model.DirectoryFormat):
     sam_files = model.FileCollection(r'*.sam', format=VCFFileFormat)
@@ -46,22 +49,30 @@ class NASP2MatrixFileFormat(model.TextFileFormat):
     def _validate_(self, level):
         pass
 
+
 class NASP2MatrixFileDirFmt(model.DirectoryFormat):
     sam_files = model.FileCollection(r'*.sam', format=NASP2MatrixFileFormat)
-
 
 
 class YAMLFileFormat(model.TextFileFormat):
     def _validate_(self, level):
         pass
 
+
 class YAMLFileDirFmt(model.DirectoryFormat):
     config_file = model.File(r'*.yaml', format=YAMLFileFormat)
+
 
 class XMLFileFormat(model.TextFileFormat):
     def _validate_(self, level):
         pass
 
+
 class XMLFileDirFmt(model.DirectoryFormat):
     config_file = model.File(r'*.xml', format=XMLFileFormat)
 
+
+plugin.register_formats(BWAIndexFileFormat, BWAIndexDirFmt, SAMFileFormat,
+                        SAMFileDirFmt, BAMFileFormat, BAMFileDirFmt, VCFFileFormat,
+                        VCFFileDirFmt, NASP2MatrixFileFormat, NASP2MatrixFileDirFmt,
+                        YAMLFileFormat, YAMLFileDirFmt, XMLFileFormat, XMLFileDirFmt)
--- q2_nasp2_types/types.py
+++ q2_nasp2_types/types.py
@@ -7,6 +7,7 @@ from q2_nasp2_types.plugin_setup import plugin
 
 # Semantic Types
 BWAIndex = SemanticType('BWAIndex')
+plugin.register_semantic_types(BWAIndex)
 
 AlignedReads = SemanticType('AlignedReads', variant_of=[FeatureData.field['type']])
 
@@ -14,25 +15,22 @@ SNPFile = SemanticType('SNPFile', field_names='type')
 VCFFile = SemanticType('VCFFile', variant_of=[SNPFile.field['type']])
 NASP2MatrixFile = SemanticType('NASPMatrixFile', variant_of=[SNPFile.field['type']])
 
-
 Config = SemanticType('Config', field_names='type')
 YAMLFile = SemanticType('YAMLFile', variant_of=[Config.field['type']])
 XMLFile = SemanticType('XMLFile', variant_of=[Config.field['type']])
 
 # Registering Types
-plugin.register_semantic_types(BWAIndex, AlignedReads)
-plugin.register_formats(SAMFileDirFmt, BAMFileDirFmt, BWAIndexDirFmt)
+
+plugin.register_semantic_types(AlignedReads, BWAIndex)
 plugin.register_semantic_type_to_format(BWAIndex, artifact_format=BWAIndexDirFmt)
 plugin.register_semantic_type_to_format(FeatureData[AlignedReads], artifact_format=SAMFileDirFmt)
 plugin.register_semantic_type_to_format(FeatureData[AlignedReads], artifact_format=BAMFileDirFmt)
 
 plugin.register_semantic_types(SNPFile, NASP2MatrixFile, VCFFile)
-plugin.register_formats(NASP2MatrixFileDirFmt, VCFFileDirFmt)
 plugin.register_semantic_type_to_format(SNPFile[NASP2MatrixFile], artifact_format=NASP2MatrixFileDirFmt)
 plugin.register_semantic_type_to_format(SNPFile[VCFFile], artifact_format=VCFFileDirFmt)
 
 plugin.register_semantic_types(Config, YAMLFile, XMLFile)
-plugin.register_formats(YAMLFileDirFmt, XMLFileDirFmt)
 
 plugin.register_semantic_type_to_format(Config[XMLFile], artifact_format=XMLFileDirFmt)
 plugin.register_semantic_type_to_format(Config[YAMLFile], artifact_format=YAMLFileDirFmt)
